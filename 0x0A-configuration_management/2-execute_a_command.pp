1475b950c2a7e1bd27f52d7bd8340cb29fef5d1d
Benson Kamau <benkabenson@gmail.com>
2023-01-11 15:07:31 +0000

    added more information on command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,7 @@
 # This file is creating a manifest that kills a process named killmenow
 
 exec { 'killmenow':
-    command     => 'pkill -f',
-    path        => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-    subscribe   => File['/root/Backup/deploy'],
-    refreshonly => true,
+    command  => 'pkill killmenow',
+    path     => '/usr/bin',
+    provider => 'shell'
 }
