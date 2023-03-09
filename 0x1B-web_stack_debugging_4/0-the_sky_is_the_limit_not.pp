79899bf59ff7c63bbd53349face403dd0e749470
Benson Kamau <benkabenson@gmail.com>
2023-03-09 18:10:58 +0000

    wait

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# This script is Fixing the nginx requests limit
+exec { 'upgrade':
+  path    => '/bin/',
+  command => 'sed -i "s/15/4096" /etc/default/nginx',
+}
+
+exec { 'restart':
+  path    => '/usr/bin/',
+  command => 'service nginx restart',
+}
